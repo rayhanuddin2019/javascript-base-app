300b0e2da977fbef9b0a4c472687c2fb15769a64
Rayhan Uddin <rayhanuddin.it.2019@gmail.com>
2023-10-23 17:33:59 +0600

    blog

--- src/examples/my-app-filter.js
+++ src/examples/my-app-filter.js
@@ -18,7 +18,7 @@ var tCore = dFanatics.getInstance({
                   loadmore    : document.querySelector('.ml-blog-view-more'),
                   reset       : document.querySelector('.reset_all_location_filter'),
                   popup_box   : document.querySelector('.location_detail_filter_box'),
-                  search_count: document.querySelector('.search-result-count')
+                  search_count: document.querySelector('.search-result-count')                 
                 },
                 request_options: {
                   action  : 'blog_filter_action',
@@ -86,74 +86,7 @@ var tCore = dFanatics.getInstance({
                   });                  
                
                 }
-              };
-            // search by name
-            var title_component = {
-              name     : "search_by_name",
-              container: document.querySelector(".ml-filter-content-area"),
-              dom      : {
-                              
-              },
-              request_options: {
-                action: 'blog_auto_complete_action',
-              },
-              // observable
-              model: {
-                blog_content  : [],
-                filter_options: {}
-              },
-              view(model) {
-                const titleHTML = model.blog_content.reduce(
-                  (html, post) => html + `<div class="blog_style_wrap">
-                  <div class = "blog_style_img position-relative w-100">
-                  <a   href  = "#"><img class = "w-100 blog_main_img" decoding = "async" src = "https://marbslifestyle.com/wp-content/uploads/2023/05/Nosso-summer-Club-lux-lounge-area-600x400.jpeg" alt = "Finding the Perfect Balance: Luxury Family-Friendly Hotels in Marbella"></a>
-                  </div>                
-                  <div  class = "blog_style_info_bx">
-                  <div  class = "blog_style_info">
-                  <div  class = "blog_catg_name">
-                  <span class = "mlf font-s12">Accommodations</span>
-                          </div>
-  
-                          <div   class = "blog_catg_title_info typescale-style">
-                          <h6><a class = "sf make-3line" href = "#">${post.title}</a></h6>
-                          <p     class = "mlf make-2line only_desktop_view">${post.body}</p>
-                          </div>
-                      </div>
-  
-                      <div  class = "blog_bottm_style position-relative position-content-after d-flex align-center justify-content-between">
-                      <a    href  = "#" class = "read_more_btn mlf position-relative position-content-after ion-ios-arrow-forward position-content-before">Read more</a>
-                      <span class = "blog_date mlf font-s12">15 Sep 2023</span>
-                      </div>
-                  </div>
-              </div>`,
-                  ""
-                );
-                return `${titleHTML}`;
-              },          
-              async controller(component) {                
-                // data canbe update from rest api / ajax
-                var url = 'https://jsonplaceholder.typicode.com/posts';
-                    url = url + "?" + new URLSearchParams(Object.assign(component.request_options,component.model.filter_options)).toString();
-              
-                await fetch(url, {
-                  method     : "GET",    // *GET, POST, PUT, DELETE, etc.  
-                  mode       : "cors",
-                  credentials: "omit",   // include, *same-origin, omit
-                  headers    : {
-                    "Content-Type": "application/json",
-                     //'Content-Type': 'application/x-www-form-urlencoded',
-                  },
-                  redirect      : "follow",        // manual, *follow, error
-                  referrerPolicy: "no-referrer",   // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                  //body: Object.assign(model.request_options,model.filter_options), // body data type must match "Content-Type" header
-                }).then(function(res){
-                  return res.json();
-                }).then(function(data){
-                  component.model.blog_content = data;
-                });                  
-             
-              }
-            };            
+            };                      
              // search by name
             var subcategory_component = {
               name     : "subcategory_load",
@@ -203,8 +136,7 @@ var tCore = dFanatics.getInstance({
              
               }
             }; 
-            sandbox.addComponent(post_component);
-            sandbox.addComponent(title_component);
+            sandbox.addComponent(post_component);           
             sandbox.addComponent(subcategory_component);
             sandbox.router();
         });
@@ -237,10 +169,10 @@ var tCore = dFanatics.getInstance({
           sandbox.componentsByName['blog_filter'].model.filter_options    = {};
           sandbox.componentsByName['blog_filter'].request_options['page'] = 1;
           sandbox.showComponent('blog_filter');
-        });
+        });      
         // mobile     
         dFanatics.Utils.On('click','.all-filter-btn', sandbox.mobileOpenCloseCallback.bind({open: 1})); 
-        dFanatics.Utils.On('click','.search-close-btn,.filter-search-btn', sandbox.mobileOpenCloseCallback.bind({open: 0}));          
+        dFanatics.Utils.On('click','.search-close-btn,.filter-search-btn', sandbox.mobileOpenCloseCallback.bind({open: 0}));  
       };
 
       sandbox.mobileOpenCloseCallback = function(e){
@@ -253,6 +185,79 @@ var tCore = dFanatics.getInstance({
       } 
     return sandbox;
   });
+
+  /**
+   *
+   * Register Core module
+   **/
+  tCore.register("blog:filter:searchbyname", function (sandbox, moduleSettings) {
+    sandbox.init = function () {
+      window.addEventListener("DOMContentLoaded", function () {
+          // search by name
+          var title_component = {
+            name     : "search_by_name",
+            container: document.querySelector(".post-title-search"),
+            dom      : {
+              search_by_name : document.querySelector(".ml-search-by-name"),                          
+            },
+            request_options: {
+              action: 'blog_auto_complete_action',
+            },
+            // observable
+            model: {
+              title_content  : [],
+              filter_options: {}
+            },
+            view(model) {
+              const titleHTML = model.title_content.reduce(
+                (html, post) => html + `<li class="blue-color"><a data-loc-name="" class="location_title blue-color" href="#">${post.title}</a></li>`,
+                ""
+              );
+              return `${titleHTML}`;
+            },          
+            async controller(component) {                
+              // data canbe update from rest api / ajax
+              var url = 'https://jsonplaceholder.typicode.com/posts';
+                  url = url + "?" + new URLSearchParams(Object.assign(component.request_options,component.model.filter_options)).toString();
+            
+              await fetch(url, {
+                method     : "GET",    // *GET, POST, PUT, DELETE, etc.  
+                mode       : "cors",
+                credentials: "omit",   // include, *same-origin, omit
+                headers    : {
+                  "Content-Type": "application/json",
+                   //'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect      : "follow",        // manual, *follow, error
+                referrerPolicy: "no-referrer",   // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                //body: Object.assign(model.request_options,model.filter_options), // body data type must match "Content-Type" header
+              }).then(function(res){
+                return res.json();
+              }).then(function(data){
+                component.model.title_content = data;
+                console.log(data); 
+              });                  
+           
+            }
+          };         
+
+          sandbox.addComponent(title_component);        
+          sandbox.router();
+      });
+  };
+
+  sandbox.router = function () {       
+      // search_by_name     
+      if(sandbox.componentsByName['search_by_name']){     
+        sandbox.componentsByName['search_by_name'].dom.search_by_name.addEventListener('keyup', function(e){
+          sandbox.componentsByName['search_by_name'].model.filter_options.text    = e.target.value;
+          sandbox.showComponent('search_by_name'); 
+        });
+      }       
+    };
+  
+  return sandbox;
+});
 
   tCore.register("dom:select", function (sandbox, moduleSettings) {
     sandbox.selectOptionCallback = function(event){
